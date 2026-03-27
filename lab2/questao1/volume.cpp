a9832a453ada480a7e7d6777ac6f94d9c9deafef
julia <juliabarbosa@poli.ufrj.br>
2026-03-27 16:40:19 -0300

    atualiza lab2 - questao 1

---
+++ lab2/questao1/volume.cpp
@@ -0,0 +1,45 @@
+//
+// Created by julia on 27/03/2026.
+//
+
+#include "volume.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Paralelepipedo::Paralelepipedo(int x, int y, int z) {
+    setVolume(x,y,z);
+}
+
+void Paralelepipedo::setVolume(int v1, int v2, int v3) {
+    if (v1 < 0 ) {x = 5;}
+    else if (v2 < 0 ) {y = 5;}
+    else if (v3 < 0 ) {z = 5;}
+    else {x = v1, y = v2, z = v3;}
+}
+
+int Paralelepipedo::calculaVolume() {
+    return x * y * z;
+}
+
+int Paralelepipedo::getVolume() {
+    return calculaVolume();
+}
+
+
+int main() {
+    int dx, dy, dz;
+    cout<<"Digite x: ";
+    cin>>dx;
+
+    cout<<"Digite y: ";
+    cin>>dy;
+
+    cout<<"Digite z: ";
+    cin>>dz;
+
+    Paralelepipedo paralelepipedo(dx,dy,dz);
+
+    cout<<"O volume do paralelepipedo é: " << paralelepipedo.getVolume();
+}
---
+++ lab2/questao1/volume.h
@@ -0,0 +1,24 @@
+//
+// Created by julia on 27/03/2026.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Paralelepipedo {
+    public:
+        Paralelepipedo(int, int, int);
+        void setVolume(int, int, int);
+        int getVolume();
+        int calculaVolume();
+
+    private:
+        int x,y,z;
+
+};
+
+
+
+
